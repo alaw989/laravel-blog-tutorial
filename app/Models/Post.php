6eac2d509420e2099832ca4674e8f4d46e1a0e03
austinlaw <austin.law@mile6.com>
2022-10-25 09:14:23 -0400

    initial commit

---
+++ app/Models/Post.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+    protected $with = ['category', 'author'];
+
+    public function scopeFilter($query, array $filters) { 
+
+        $query->when($filters['search'] ?? false, function ($query, $search) {
+            $query
+            ->where('title', 'like', '%' . $search . '%')
+            ->orWhere('body', 'like', '%' . $search . '%');
+        });
+
+        $query->when($filters['category'] ?? false, fn ($query, $category) =>
+            $query
+                ->whereExists(fn($query) => 
+                $query->from('categories')
+                    ->where('categories.id', 'posts.category_id')
+                    ->where('categories.slug', $category))
+                );
+
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function author() {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
+
+
